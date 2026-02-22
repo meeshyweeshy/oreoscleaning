70b720dca53c94eb33e12ed49a543423147a1b62
meeshyweeshy <kloudzdhc@gmail.com>
2026-02-21 21:24:46 -0500

    Fix asset paths for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    // If you are deploying to a custom domain (like oreocleaning.com), base should be '/'
-    // If you are deploying to github.io/repo-name/, base should be '/repo-name/'
-    base: '/',
+    // Using './' makes paths relative, which works for both 
+    // github.io/repo-name/ and custom domains.
+    base: './',
     build: {
         outDir: 'dist',
     }
